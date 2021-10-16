15c3f8a7b7714c6a3053ddb41df399b41ec88079
“nadav <nadavg1000@gmail.com>
2021-10-16 21:46:28 +0300

    finished validating user inputs

--- front-react/src/components/common/myInput.jsx
+++ front-react/src/components/common/myInput.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const MyInput = ({name, ...rest}) => {
+const MyInput = ({name,error, ...rest}) => {
     return ( 
+        <React.Fragment>
         <input {...rest} name={name} id={name} className="form-control" />
-     );
+         {error && <span className="text-danger">{error}</span>}
+         </React.Fragment>
+        );
 }
  
 export default MyInput;
--- front-react/src/components/wizardBuilder/wizard.jsx
+++ front-react/src/components/wizardBuilder/wizard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Joi from "joi-browser";
 import Header from "../common/header";
 import Label from "../common/label";
 import MyInput from "../common/myInput";
@@ -13,62 +14,115 @@ const types = [
   { label: "number", value: "number" },
 ];
 
-
-
 const Wizard = () => {
   const [questionNum, setQuestionNum] = useState(1);
   const [inputType, SetInputType] = useState(types[0].value);
-const [data, setData] = useState({
-    fieldLabel:"", 
-    inputName:"",
-})
+  const [data, setData] = useState({
+    fieldLabel: "",
+    inputName: "",
+  });
+  const [errors, setErrors] = useState({});
 
-// handle changed inputs on field label & input name
-function handleChange(event){
+  //create schema to validate user input
+  let schema = {
+    fieldLabel: Joi.string().required().min(2).label("fieldLabel"),
+    inputName: Joi.string().required().min(2).label("inputName"),
+    inputType: Joi.string().label("inputType"),
+  };
+
+  const validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    schema = { [name]: schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  
+  const validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(data, schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  // handle changed inputs on field label & input name
+  function handleChange(event) {
     const { name, value } = event.target;
+    const errorMessage = validateProperty(event.target);
+    console.log(errorMessage, "ff");
+    if (errorMessage){
+        errors[name] = errorMessage;
+        setErrors(errors)
+        console.log("t",errors);
+    }
+     else delete errors[name];
+    
     setData((prevState) => {
       return {
         ...prevState,
         [name]: value,
       };
     });
- }
+  }
 
-//  add 1 to question number and reset values for fields
- function nextQuestion(){
-    // create a new array to insert the state data to
-    let questions=[];
+  // create a new array to insert the state data to
+  let questions = [];
+
+  function nextQuestion() {
+    let existingQuestions = JSON.parse(localStorage.getItem("Questions"));
     // create an object from the user inputs
-    // and push it to the array
-    questions.push({"question number":questionNum, 
-    "fieldLabel":data.fieldLabel,
-    "inputName":data.inputName,
-    "inputType":inputType
-})
-    console.log(questions);
-     localStorage.setItem('questions', JSON.stringify(questions))
+    let questionData = {
+      "question number": questionNum,
+      fieldLabel: data.fieldLabel,
+      inputName: data.inputName,
+      inputType: inputType,
+    };
+
+    if (existingQuestions == null) {
+      //if theres no questions in local storage  push the object to the array
+      questions.push(questionData);
+      localStorage.setItem("Questions", JSON.stringify(questions));
+    } else {
+      //if there were previous questions,
+      //get the array thaat holds the questions object and push the new
+      //question into it
+      existingQuestions.push(questionData);
+      localStorage.setItem("Questions", JSON.stringify(existingQuestions));
+    }
+
+    //  add 1 to question number and reset values for fields
     setQuestionNum(questionNum + 1);
-    data.fieldLabel="";
-    data.inputName="";
-    SetInputType(types[0].value)
- }
+    data.fieldLabel = "";
+    data.inputName = "";
+    SetInputType(types[0].value);
+  }
+ 
   return (
     <div className="container">
       <Header titleText="Form Builder" />
       <h4>Question number {questionNum}</h4>
       <div className="form-group col-4">
         <Label name="fieldLabel" label="Field Label" />
-        <MyInput name="fieldLabel"
-         type="text" 
-         value={data.fieldLabel}
-         onChange={handleChange} />
+        <MyInput
+          name="fieldLabel"
+          type="text"
+          value={data.fieldLabel}
+          onChange={handleChange}
+           error={errors.fieldLabel}
+        />
       </div>
       <div className="form-group col-4">
         <Label name="inputName" label="Input Name" />
-        <MyInput name="inputName" 
-        type="text"
-        value={data.inputName}
-        onChange={handleChange}/>
+        <MyInput
+          name="inputName"
+          type="text"
+          value={data.inputName}
+          onChange={handleChange}
+          error={errors.inputName}
+        />
       </div>
       <Select
         name="inputType"
@@ -90,7 +144,8 @@ function handleChange(event){
         <button
           type="button"
           className="btn btn-primary"
-          onClick={() =>nextQuestion() }
+          onClick={() => nextQuestion()}
+          disabled={validate()}
         >
           Next Question
         </button>
